36d1642208adafc03d66b05e2ef17ef71bcb77a7
sandeepk-480 <sandeepkanojiya480@gmail.com>
2023-12-11 13:32:00 +0530

    updated css with media query, and responsive unit

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 class SignUpForm(UserCreationForm):
-    Address_line_1 = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 2}), required=False)
+    Address_line_1 = forms.CharField(widget=forms.TextInput, required=False)
     profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'accept': 'images/*'}), required=False)
     class Meta:
         model = CustomUser
